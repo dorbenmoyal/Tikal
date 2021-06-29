9e9fc8b4e5e3a4d6ae22a0cd5e9feba9e62840df
dorbenmoyal <38722866+dorbenmoyal@users.noreply.github.com>
2021-06-29 20:55:57 +0300

    Adding PostMan Collection

---
+++ Unitests/MessagesExamples.js
@@ -0,0 +1,34 @@
+const messagesExample = 
+[
+    {
+        "sender": "dbmm@gmail.com",
+        "recipient": "dbds2mm@gmail.com",
+        "message": "This is a Message"
+    },
+    {
+        "sender": "dbmasdf",
+        "recipient": "dbd",
+        "message": "Message Example"
+    },
+    {
+        "sender": "12312",
+        "recipient": "asdf",
+        "message": "This is Tetst"
+    },
+    {
+        "sender": "dbmm@gmail.com",
+        "recipient": "dbds2mm@gmail.com",
+        "message": "This is a Message"
+    },
+    {
+        "sender": "dbmm@gmail.com",
+        "recipient": "dbds2mm@gmail.com",
+        "message": "This is a Message"
+    },
+    {
+        "sender": "dbmm@gmail.com",
+        "recipient": "dbds2mm@gmail.com",
+        "message": "This is a Message"
+    },
+
+]
